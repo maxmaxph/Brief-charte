9d213f8f2b088e54fe53149d141870cd35c99b98
maxmaxph <maxmaxph@gmail.com>
2023-06-19 11:33:13 +0200

    shadow + button + w3c

--- js/script.js
+++ js/script.js
@@ -1,31 +1,45 @@
-const btnRandom = document.getElementById('btn-random');  
+const btnRandom = document.getElementById("btn-random");
 
-let myArray = ['Alexandre','Beyram','Fabrice','Guillaume','Jérome', 'Lydie','Loïc.G','Loïc.V','Maxime','Mélina','Nicolas','Quentin','Rémy','Samir','Samra','Thomas'];   
+let myArray = [
+  "Alexandre",
+  "Beyram",
+  "Fabrice",
+  "Guillaume",
+  "Jérome",
+  "Lydie",
+  "Loïc.G",
+  "Loïc.V",
+  "Maxime",
+  "Mélina",
+  "Nicolas",
+  "Quentin",
+  "Rémy",
+  "Samir",
+  "Samra",
+  "Thomas",
+];
 
-btnRandom.addEventListener('click', () => {
-  RandArray(myArray) 
-  });  
+btnRandom.addEventListener("click", () => {
+  RandArray(myArray);
+});
 
-function RandArray(array){
-let rand = Math.random()*array.length | 0;
-console.log(Math.floor(array));
-let rValue = array[rand];     
-console.log(rValue);     
-btnRandom.innerHTML = rValue;
-setTimeout(reinitialiserBouton, 3000);
+function RandArray(array) {
+  let rand = (Math.random() * array.length) | 0;
+  console.log(Math.floor(array));
+  let rValue = array[rand];
+  console.log(rValue);
+  btnRandom.innerHTML = rValue;
+  setTimeout(reinitialiserBouton, 3000);
+}
 
-};
-
-function reinitialiserBouton(){
-  document.getElementById("btn-random").innerHTML= "Random apprenant";
+function reinitialiserBouton() {
+  document.getElementById("btn-random").innerHTML = "Random apprenant";
 }
 
 function desactiverBouton() {
-
-  
   btnRandom.disabled = true; // Désactive le bouton
-  
-  setTimeout(function() {
+
+  setTimeout(function () {
     btnRandom.disabled = false; // Réactive le bouton après 3 secondes
   }, 3000); // 3000 millisecondes = 3 secondes
-};
+}
